21a8f6da192f4d38cafee9681a56f1afd7f4e560
Akhilesh Anand Undralla <akhil.au@Mac.lan>
2025-12-30 15:14:18 -0500

    new rule 1

--- main.tf
+++ main.tf
@@ -1,5 +1,22 @@
 resource "google_storage_bucket" "my_test_bucket" {
   name          = "my-policy-test-bucket-unique-name"
-  location      = "europe-west1"
+  location      = "US"
   force_destroy = true
+}
+
+
+resource "google_compute_instance" "expensive_server" {
+  name         = "budget-breaker"
+  machine_type = "n2-standard-16"  # <--- This is a generic high-cpu machine
+  zone         = "us-central1-a"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
 }
